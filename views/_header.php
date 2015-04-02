5a956c83ebe8f34f63d8cfa729101350145fbf95
svs-websoft <svs-websoft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-02 13:44:30 +0000

    donate button added
    git-svn-id: https://plugins.svn.wordpress.org/svs-shortlink-analytics/trunk@1126336 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/_header.php
+++ views/_header.php
@@ -2,4 +2,13 @@
     <script src="//code.jquery.com/ui/1.11.3/jquery-ui.js"></script>
     <link rel="stylesheet" href="//code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css">
     <link rel="stylesheet" href="<?php echo plugins_url('../style/css_svs.css', __FILE__ )?> ">
-    <h2>SVS ShortLink Statistics</h2>
+    <h2 style="float:left;">SVS ShortLink Statistics</h2>
+      <div style="float:right;padding-top:10px;">
+    	<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+		<input type="hidden" name="cmd" value="_s-xclick">
+		<input type="hidden" name="hosted_button_id" value="4C652FM4GK3UW">
+		<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+		<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+		</form>
+    </div>
+    <div style="clear:both">&nbsp;</div>
